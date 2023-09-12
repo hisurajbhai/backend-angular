ccfcf5fc3638837bd3618b4de280a2da887c4ab4
hisurajbhai <sj4147@srmist.edu.in>
2023-09-13 01:24:19 +0530

    done

--- index.js
+++ index.js
@@ -13,90 +13,91 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL: ' + err.message);
-    return;
-  }
-  console.log('Connected to MySQL');
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.message);
+        return;
+    }
+    console.log('Connected to MySQL');
 });
 
 // Registration Route
 app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  try {
-    // Check if the username already exists in the database
-    const checkUsernameQuery = 'SELECT * FROM users WHERE username = ?';
-    const [existingUser] = await db.promise().query(checkUsernameQuery, [username]);
+    try {
+        // Check if the username already exists in the database
+        const checkUsernameQuery = 'SELECT * FROM users WHERE username = ?';
+        const [existingUser] = await db.promise().query(checkUsernameQuery, [username]);
 
-    if (existingUser.length > 0) {
-      return res.status(400).json({ message: 'Username already exists' });
+        if (existingUser.length > 0) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Insert the user into the database
+        const createUserQuery = 'INSERT INTO users (username, password) VALUES (?, ?)';
+        await db.promise().query(createUserQuery, [username, hashedPassword]);
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error('Error during registration:', error);
+        res.status(500).json({ message: 'Registration failed' });
     }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Insert the user into the database
-    const createUserQuery = 'INSERT INTO users (username, password) VALUES (?, ?)';
-    await db.promise().query(createUserQuery, [username, hashedPassword]);
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error('Error during registration:', error);
-    res.status(500).json({ message: 'Registration failed' });
-  }
 });
 
 // Login Route
 app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  try {
-    // Check if the username exists in the database
-    const getUserQuery = 'SELECT * FROM users WHERE username = ?';
-    const [users] = await db.promise().query(getUserQuery, [username]);
+    try {
+        // Check if the username exists in the database
+        const getUserQuery = 'SELECT * FROM users WHERE username = ?';
+        const [users] = await db.promise().query(getUserQuery, [username]);
 
-    if (users.length === 0) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+        if (users.length === 0) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const user = users[0];
+
+        // Compare the provided password with the hashed password in the database
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        // Generate a JWT token
+        const token = jwt.sign({ userId: user.id, username: user.username }, process.env.JWT_SECRET, {
+            expiresIn: '1h', // Token expires in 1 hour
+        });
+
+        res.status(200).json({ token });
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ message: 'Login failed' });
     }
-
-    const user = users[0];
-
-    // Compare the provided password with the hashed password in the database
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    // Generate a JWT token
-    const token = jwt.sign({ userId: user.id, username: user.username }, process.env.JWT_SECRET, {
-      expiresIn: '1h', // Token expires in 1 hour
-    });
-
-    res.status(200).json({ token });
-  } catch (error) {
-    console.error('Error during login:', error);
-    res.status(500).json({ message: 'Login failed' });
-  }
 });
 
 // Protected Route for Fetching User Profile
 app.get('/profile', authenticateToken, (req, res) => {
-  // The user object is attached to the request object by the authenticateToken middleware
-  const { userId, username } = req.user;
+    // The user object is attached to the request object by the authenticateToken middleware
+    const { userId, username } = req.user;
 
-  // You can fetch the user's profile information from the database here
-  // Example: const userProfile = fetchUserProfileFromDatabase(userId);
+    // You can fetch the user's profile information from the database here
+    // Example: const userProfile = fetchUserProfileFromDatabase(userId);
 
-  res.status(200).json({ userId, username });
+    res.status(200).json({ userId, username });
 });
 
 app.get('/', (req, res) => {
@@ -104,16 +105,16 @@ app.get('/', (req, res) => {
 });
 // Middleware to authenticate JWT token
 function authenticateToken(req, res, next) {
-  const token = req.header('Authorization');
-  if (token == null) return res.status(401).json({ message: 'Authentication required' });
+    const token = req.header('Authorization');
+    if (token == null) return res.status(401).json({ message: 'Authentication required' });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Invalid token' });
-    req.user = user;
-    next();
-  });
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
 }
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
